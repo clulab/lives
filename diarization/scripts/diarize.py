9c95c14234600c62876c371036b29b7e33aacae5
john culnan <jmculnan@email.arizona.edu>
2022-12-02 14:56:55 -0700

    updates for newever pyannote audio version

--- diarization/scripts/diarize.py
+++ diarization/scripts/diarize.py
@@ -16,17 +16,19 @@ if __name__ == "__main__":
         ]
     )
 
-    if len(sys.argv) >= 3:
+    if len(sys.argv) >= 4:
+        # access token for huggingface
+        ACCESS_TOKEN = sys.argv[1]
         # directory containing audio files
-        AUDIO_DIR = sys.argv[1]
+        AUDIO_DIR = sys.argv[2]
 
         # a csv file containing a list of the files to diarize
         # assumes list of files is the first column in this file
         # NOTE: do not include extensions in file names
-        FILES_TO_DIARIZE = sys.argv[2]
+        FILES_TO_DIARIZE = sys.argv[3]
 
-        if len(sys.argv) > 3:
-            SAVE_DIR = sys.argv[3]
+        if len(sys.argv) > 4:
+            SAVE_DIR = sys.argv[4]
         else:
             SAVE_DIR = f"{AUDIO_DIR}/../diarized_files"
     else:
@@ -37,5 +39,10 @@ if __name__ == "__main__":
     f_to_diarize = to_diarize.iloc[:, 0].tolist()
 
     # diarize the files
-    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization")
+    # pyannote now requires you to sign in to use their models
+    # create an access token on huggingface and use this here
+    # you also need to give your information on each of the models used in the pipeline
+    # you do NOT need to be affiliated with pyannote to do this
+    # this ONLY works with pyannote.audio v 2.1+
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token=ACCESS_TOKEN)
     diarize_n_files(pipeline, f_to_diarize, AUDIO_DIR, SAVE_DIR)
