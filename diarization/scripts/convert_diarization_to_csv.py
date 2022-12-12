34a40cee48b89b9a8cba2308614b6e3ac12382c1
john culnan <jmculnan@email.arizona.edu>
2022-12-12 09:34:25 -0800

    updating code to convert diarizations to csv and chunk audio files

--- diarization/scripts/convert_diarization_to_csv.py
+++ diarization/scripts/convert_diarization_to_csv.py
@@ -4,6 +4,7 @@
 
 from pathlib import Path
 import pandas as pd
+import subprocess as sp
 # import os
 
 
@@ -20,6 +21,8 @@ class RTTMConverter:
         # create directories in o_path if they don't yet exist
         self.o_path.mkdir(parents=True, exist_ok=True)
 
+        self.dfs_dict = self.read_in_rttm()
+
     def read_in_rttm(self):
         # read in all rttms
         # save in dict
@@ -28,22 +31,62 @@ class RTTMConverter:
         for f in self.i_path.iterdir():
             if f.suffix == ".rttm":
                 rttm_df = pd.read_csv(f,
-                                      colnames=["p1", "fname",
-                                                "p2", "turn_start",
-                                                "turn_end", "p3",
-                                                "p4", "speaker",
-                                                "p5", "p6"])
+                                      names=["p1", "fname",
+                                             "p2", "turn_start",
+                                             "turn_length", "p3",
+                                             "p4", "speaker",
+                                             "p5", "p6"], sep=" ")
+
                 # subset only necessary columns
-                rttm_df = rttm_df[["fname", "turn_start", "turn_end", "speaker"]]
+                rttm_df = rttm_df[["fname", "turn_start", "turn_length", "speaker"]]
+
+                # add .wav to end of fname for use with whisper scripts
+                rttm_df.fname = rttm_df.fname.apply(lambda x: x + ".wav")
+
+                # calculate turn end from length of turn
+                rttm_df['turn_end'] = rttm_df['turn_start'] + rttm_df['turn_length']
 
                 # calculate time between turns
-                rttm_df['time_since_last'] = rttm_df["turn_start"].shift(-1) - rttm_df["turn_end"]
+                rttm_df['time_to_next'] = rttm_df["turn_start"].shift(-1) - rttm_df["turn_end"]
 
-                # ID turns that are close together and by same speaker
-                # and collapse over these
+                # todo: ID turns that are close together and by same speaker
+                #   and collapse over these
 
                 dfs_dict[f.stem] = rttm_df
 
         return dfs_dict
 
+    def chunk_wav_files(self, wav_read_pathstr, wav_save_pathstr):
+        # create output directory
+        wav_save_path = Path(wav_save_pathstr)
+        wav_save_path.mkdir(parents=True, exist_ok=True)
+
+        for item in self.dfs_dict.keys():
+            # create subdir for this wav file
+            itemname = item.split(".wav")[0]
+            itempath = wav_save_path / itemname
+            itempath.mkdir(exist_ok=True)
+
+            # get the relevant df out of df dict
+            df = self.dfs_dict[item]
+
+            # for each line in this df:
+            for row in df.itertuples():
+
+                # chunk using start and end times in df
+                sp.run(["ffmpeg","-ss", str(row.turn_start),  "-i", f"{wav_read_pathstr}/{row.fname}",
+                        "-t", str(row.turn_length), "-c", "copy",
+                        f"{str(itempath)}/{itemname}_{str(row.turn_start)}-{str(row.turn_end)}.wav"])
+
     # def save_to_
+
+if __name__ == "__main__":
+    rttmpath = "/media/jculnan/datadrive/lives_data_copy/diarized_files"
+    savepath = "/media/jculnan/datadrive/lives_data_copy/diarized_json"
+
+    x = RTTMConverter(rttmpath, savepath)
+    x.read_in_rttm()
+    x.chunk_wav_files(
+        wav_read_pathstr="/media/jculnan/datadrive/lives_data_copy/Call recordings, all fidelity scored calls, n=323",
+        wav_save_pathstr="/media/jculnan/datadrive/lives_data_copy/split_audio"
+    )
