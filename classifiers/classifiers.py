a17e17554c4251512059fd51a1b307d121327e9a
Steven Bethard <bethard@email.arizona.edu>
2023-05-09 15:25:54 -0700

    Continues work on huggingface dataset for LIvES utterance annotations

--- classifiers/classifiers.py
+++ classifiers/classifiers.py
@@ -1,6 +1,8 @@
 import argparse
+import collections
 import json
 import os.path
+import datasets
 import whisper
 
 
@@ -10,30 +12,53 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def construct_dataset(project_json_path, wav_file_dir):
-    model = whisper.load_model("large-v2")
+def construct_dataset(project_json_path, wav_file_dir, dataset_path):
 
+    # gather LabelStudio annotations, grouping by (start, end) in the audio
+    file_annotations = collections.defaultdict(dict)
     with open(project_json_path) as json_file:
-        project_json = json.load(json_file)
-        for file_json in project_json:
+        for file_json in json.load(json_file):
             wav_file_name = file_json["file_upload"]
-            print(wav_file_name)
-            wav_path = os.path.join(wav_file_dir, wav_file_name)
-            audio = whisper.load_audio(wav_path)
+
+            # each annotation is a LabelStudio "result"
             for annotations_json in file_json["annotations"]:
+
+                # for now, use only Damian's annotations
+                annotator = annotations_json["completed_by"]["email"]
+                if annotator != "damianiji@email.arizona.edu":
+                    continue
+
                 for result_json in annotations_json["result"]:
+
+                    # LIvES utterance annotations are LabelStudio "choices"
                     if result_json["type"] == "choices":
                         turn_start = result_json["value"]["start"]
                         turn_end = result_json["value"]["end"]
                         labels = result_json["value"]["choices"]
 
-                        turn_start = int(turn_start * whisper.audio.SAMPLE_RATE)
-                        turn_end = int(turn_end * whisper.audio.SAMPLE_RATE)
-                        turn_audio = audio[turn_start:turn_end]
-                        whisper_json = whisper.transcribe(model, turn_audio)
-                        text = whisper_json["text"]
+                        # save span and labels
+                        span = turn_start, turn_end
+                        if span not in file_annotations:
+                            file_annotations[wav_file_name][span] = []
+                        file_annotations[wav_file_name][span].extend(labels)
 
-                        print(f"{labels}\n{text}")
+    # create dataset from annotations and Whisper transcriptions
+    model = whisper.load_model("tiny")#"large-v2")
+    dataset = dict(text=[], labels=[])
+    for wav_file_name, annotations in file_annotations.items():
+        wav_path = os.path.join(wav_file_dir, wav_file_name)
+        audio = whisper.load_audio(wav_path)
+        for ((turn_start, turn_end), labels) in sorted(annotations.items()):
+            turn_start = int(turn_start * whisper.audio.SAMPLE_RATE)
+            turn_end = int(turn_end * whisper.audio.SAMPLE_RATE)
+            turn_audio = audio[turn_start:turn_end]
+            whisper_json = whisper.transcribe(model, turn_audio)
+            text = whisper_json["text"]
+            dataset["text"].append(text)
+            dataset["labels"].append(labels)
+
+    dataset = datasets.Dataset.from_dict(dataset)
+    dataset.save_to_disk(dataset_path)
 
 
 def train():
@@ -51,6 +76,7 @@ if __name__ == "__main__":
     dataset_parser.set_defaults(func=construct_dataset)
     dataset_parser.add_argument("project_json_path")
     dataset_parser.add_argument("wav_file_dir")
+    dataset_parser.add_argument("dataset_path")
 
     kwargs = vars(parser.parse_args())
     kwargs.pop("func")(**kwargs)
